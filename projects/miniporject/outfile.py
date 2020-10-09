87f574256082d6355953eb1cbf6bd7922b9f73d0
remytanx <remy.tan@gmail.com>
2020-10-09 17:40:57 +0800

    Update on own project

---
+++ projects/miniporject/outfile.py
@@ -0,0 +1,22 @@
+counter = 1
+print(f"Counter at {counter}.")
+
+while counter < 5:
+    print(f"Counter at {counter}.")
+
+    counter = counter + 1
+
+print(f"Counter at {counter}.")
+
+word1 = "rule"
+word2 = counter
+word3 = "header.txt"
+
+output = "Hello World"
+
+print(f"This is {output}")
+print(word1,word2,word3)
+# print(f"This is {createfile}")
+
+# target = open(createfile,'w')
+# target.close()
