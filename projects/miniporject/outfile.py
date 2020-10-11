368e0981ff0dbc40dfdcbc11c4b177ea848e22c9
remytanx <remy.tan@gmail.com>
2020-10-12 00:20:06 +0800

    Segement the code into 2 functions, number() and concent()

--- projects/miniporject/outfile.py
+++ projects/miniporject/outfile.py
@@ -1,22 +1,38 @@
-counter = 1
-print(f"Counter at {counter}.")
+print("Hello World!")
+counter = 0
 
-while counter < 5:
+def number():
+    # This is to increment the number in the filename.
+    counter = 1
     print(f"Counter at {counter}.")
 
-    counter = counter + 1
+    # This is the loop for the number of files.
+    while counter < 5:
+        print(f"Counter at {counter}.")
 
-print(f"Counter at {counter}.")
+        counter = counter + 1
 
-word1 = "luer"
-word2 = counter
-word3 = "template.txt"
 
-output = "Hello World"
+    print(f"Counter at {counter}.")
 
-print(f"This is {output}")
-print(word1,word2,word3)
-# print(f"This is {createfile}")
+    return counter
 
+# counter = number()
+
+def concat():
+    # Test strings to concentate.
+    word1 = "luer"
+    word2 = counter
+    word3 = "template.txt"
+
+    output = "Hello World"
+
+    print(f"This is {output}")
+    print(word1,word2,word3)
+    # print(f"This is {createfile}")
+
+# This is to create file and it works. Commented to test the writing and formatting of filename.
 # target = open(createfile,'w')
 # target.close()
+
+print("Bye bye World!")
