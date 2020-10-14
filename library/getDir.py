1627b1477507312b7bf8d53bebccd8629eff6b5a
remytanx <remy.tan@gmail.com>
2020-10-15 07:36:01 +0800

    Created new dir, library

---
+++ library/getDir.py
@@ -0,0 +1,27 @@
+import os
+def fileAtTerm():
+    print("Path at terminal when executing this file")
+    print(os.getcwd() + "\n")
+    print("This file path, relative to os.getcwd()")
+    print(__file__ + "\n")
+
+def fullPath():
+    print("This file full path (following symlinks)")
+    full_path = os.path.realpath(__file__)
+    print(full_path + "\n")
+    return full_path
+
+def pathNFilename(full_path):
+    print("This file directory and name")
+    path, filename = os.path.split(full_path)
+    print(path + ' --> ' + filename + "\n")
+
+def fileDir(full_path):
+    print("This file directory only")
+    print(os.path.dirname(full_path))
+
+
+fileAtTerm()
+path = fullPath()
+pathNFilename(path)
+fileDir(path)
--- projects/miniporject/getFilename.py
+++ projects/miniporject/getFilename.py
@@ -9,24 +9,9 @@ def spMsg():
 def getFilename():
     print("In fN getFilename")
     # pass
-    os.chdir("projects\miniporject")
+    os.chdir("projects\\miniporject")
     for file in glob.glob("*.txt"):
         print(file)
-    # print("Path at terminal when executing this file")
-    # print(os.getcwd() + "\n")
-    # print("This file path, relative to os.getcwd()")
-    # print(__file__ + "\n")
-
-    # print("This file full path (following symlinks)")
-    # full_path = os.path.realpath(__file__)
-    # print(full_path + "\n")
-
-    # print("This file directory and name")
-    # path, filename = os.path.split(full_path)
-    # print(path + ' --> ' + filename + "\n")
-
-    # print("This file directory only")
-    # print(os.path.dirname(full_path))
 
 stMsg()
 
