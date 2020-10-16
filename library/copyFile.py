e1a3c77ee4026fd64a9b9ffb96622e7f6b0174ff
remytanx <remy.tan@gmail.com>
2020-10-16 16:37:59 +0800

    Weird weird updates

--- library/copyFile.py
+++ library/copyFile.py
@@ -6,10 +6,13 @@
 import shutil, os, platform
 
 uname = platform.uname()
+path = ""
+
+print("Hello World!")
 
 if(uname.node == "P_IFSLT861"):
     # path = "C:\\Users\\kht79\\Documents\\Test Src Folder"
-    path = "projects\miniporject"
+    path = "projects\\miniproject"
 else:
     print("Machine not found!")
 
@@ -21,3 +24,14 @@ os.chdir(path)
 
 print("Path at terminal when executing this command")
 print(os.getcwd() + "\n")
+
+# shutil.copy2('/src/dir/file.ext', '/dst/dir/newname.ext') # complete target filename given
+# shutil.copy2('/src/file.ext', '/dst/dir') # target filename is /dst/dir/file.ext
+
+src = "projects\\miniproject\\Test Src Folder"
+dst = "projects\\miniproject\\Test Dst Folder"
+shutil.copy2(src, dst)
+
+print("Done copying...")
+
+print("Bye bye World!")
